131064465628d2beb3595733c26cc93adc4f60ee
Jaime Gerst <jaimegerst1@gmail.com>
2020-10-10 12:08:25 -0400

    Icons, Favorites, and Comments

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -14,7 +16,7 @@ const DirectoryNavigator = createStackNavigator(
     },
     {
         initialRouteName: 'Directory',
-        defaultNavigationOptions: {
+        navigationOptions: {
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
@@ -31,7 +33,41 @@ const HomeNavigator = createStackNavigator(
         Home: { screen: Home }
     },
     {
-        defaultNavigationOptions: {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        navigationOptions: {
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
@@ -46,7 +82,9 @@ const HomeNavigator = createStackNavigator(
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator }
     },
     {
         drawerBackgroundColor: '#CEC8FF'
--- shared/campsites.js
+++ shared/campsites.js
@@ -1,32 +1,32 @@
 export const CAMPSITES = [
     {
         id: 0,
-        name: "React Lake Campground",
-        image: "/assets/images/react-lake.jpg",
+        name: "Slate Shack",
+        image: "/assets/images/slate-shack.jpg",
         elevation: 1233,
         featured: false,
         description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
     },
     {
         id: 1,
-        name:"Chrome River Campground ",
-        image: "/assets/images/chrome-river.jpg",
+        name:"Hillside Habitat",
+        image: "/assets/images/Hillside-Habitat.jpg",
         elevation: 877,
         featured: false,
         description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
     },
     {
         id: 2,
-        name:"Breadcrumb Trail Campground",
-        image: "/assets/images/breadcrumb-trail.jpg",
+        name:"Enchanted Escape",
+        image: "/assets/images/enchanted-escape",
         elevation: 2901,
         featured: false,
         description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
     },
     {
         id: 3,
-        name:"Redux Woods Campground",
-        image: "/assets/images/redux-woods.jpg",
+        name:"Peaceful Palace",
+        image: "/assets/images/peaceful-palace.jpg",
         elevation: 42,
         featured: true,
         description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
